b76e437ee37e87c0c52e1d86e159b91bb297ad38
vhorielova <horielova.viktoria@gmail.com>
2025-10-04 22:24:16 +0300

    create database connection

--- src/database/Connection.hs
+++ src/database/Connection.hs
@@ -1,1 +1,14 @@
-module Connection where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Database.Connection (connectDB) where
+
+import Database.PostgreSQL.Simple
+
+connectDB :: IO Connection
+connectDB = connect defaultConnectInfo
+  { connectHost     = "localhost"
+  , connectPort     = 5432
+  , connectUser     = "postgres"
+  , connectPassword = ""
+  , connectDatabase = "inform_resourses"
+  }
