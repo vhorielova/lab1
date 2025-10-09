3e6e84e8ff15de379cbb85d546777bd427acdc7a
vhorielova <horielova.viktoria@gmail.com>
2025-10-10 02:21:23 +0300

    add linkResourceAuthor and getAllAuthors queries

--- src/database/Queries.hs
+++ src/database/Queries.hs
@@ -9,12 +9,13 @@ import Database.PostgreSQL.Simple
 getAllResources :: Connection -> IO [Resource]
 getAllResources conn = query_ conn "SELECT * FROM resources"
 
-insertResource :: Connection -> Resource -> IO ()
-insertResource conn resource = do
-  _ <- execute conn
-         "INSERT INTO resources (title, annotation, typeId, purpose, openedAt, timeOfUsing, address) VALUES (?,?,?,?,?,?,?)"
-         resource
+insertResource :: Connection -> String -> String -> Int -> String -> String -> IO Int
+insertResource conn title annotation typeId purpose address = do
+  [Only resId] <- query conn
+         "INSERT INTO resources (title, annotation, type_id, purpose, address) VALUES (?,?,?,?,?) RETURNING id"
+         (title, annotation, typeId, purpose, address)
   putStrLn "resource inserted"
+  return (resId)
 
 getAllUsers :: Connection -> IO [User]
 getAllUsers conn = query_ conn "SELECT * FROM users"
@@ -26,3 +27,14 @@ insertUser conn username = do
         (Only username)
     putStrLn "user inserted"
     return (User userId username)
+
+linkResourceAuthor :: Connection -> Int -> Int -> IO ()
+linkResourceAuthor conn resId authId = do
+  _ <- execute conn
+    "INSERT INTO resource_author (resource_id, author_id) VALUES (?,?)"
+    (resId, authId)
+  putStrLn "linked resource and author"
+
+getAllAuthors :: Connection -> IO [Author]
+getAllAuthors conn = do
+  query_ conn "SELECT id, name, surname FROM authors"
