e4d273f641f8598a5e590d35009d7ebd004e3678
vhorielova <horielova.viktoria@gmail.com>
2025-10-09 15:58:24 +0300

    extend database connection

--- src/database/Connection.hs
+++ src/database/Connection.hs
@@ -1,14 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Database.Connection (connectDB) where
+module Connection
+    ( connectDB
+    , closeDB
+    ) where
 
 import Database.PostgreSQL.Simple
 
 connectDB :: IO Connection
-connectDB = connect defaultConnectInfo
-  { connectHost     = "localhost"
-  , connectPort     = 5432
-  , connectUser     = "postgres"
-  , connectPassword = ""
-  , connectDatabase = "inform_resourses"
-  }
+connectDB = do
+    conn <- connect defaultConnectInfo
+        { connectHost = "localhost"
+        , connectDatabase = "inform_resources"
+        , connectUser = "postgres"
+        , connectPassword = ""
+        }
+    putStrLn "Connected to PostgreSQL!"
+    return conn
+
+closeDB :: Connection -> IO ()
+closeDB conn = do
+    close conn
+    putStrLn "Connection closed."
