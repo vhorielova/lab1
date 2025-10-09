f36a655f78ea7fa835e75834d3264bcaf25f4fdb
vhorielova <horielova.viktoria@gmail.com>
2025-10-09 21:33:59 +0300

    create Login

---
+++ src/app/Login.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Login
+  ( mainMenu
+  ) where
+
+import Database.PostgreSQL.Simple
+import UserActions (doLogin, doCreateAccount)
+import System.IO (hFlush, stdout)
+
+prompt :: String -> IO String
+prompt text = do
+    putStr text
+    putStr " "
+    hFlush stdout
+    getLine
+
+mainMenu :: Connection -> IO ()
+mainMenu conn = do
+    putStrLn "\nDo you want to log in or create an account?"
+    putStrLn "1 - Log in"
+    putStrLn "2 - Create account"
+    putStrLn "3 - Exit"
+    choice <- prompt "Enter your choice:"
+
+    case choice of
+        "1" -> doLogin conn
+        "2" -> doCreateAccount conn
+        "3" -> putStrLn "Goodbye!"
+        _   -> putStrLn "Invalid option." >> mainMenu conn
