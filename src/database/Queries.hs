02669a3dcc040699f6daf2c9c2dfb694691b809a
vhorielova <horielova.viktoria@gmail.com>
2025-10-09 16:53:06 +0300

    add insertResource, getAllUsers and insertUser queries

--- src/database/Queries.hs
+++ src/database/Queries.hs
@@ -4,3 +4,20 @@ import Models
 
 getAllResources :: Connection -> IO [Resource]
 getAllResources conn = query_ conn "SELECT * FROM resources"
+
+insertResource :: Connection -> Resource -> IO ()
+insertResource conn resource = do
+  _ <- execute conn
+         "INSERT INTO resources (title, annotation, typeId, purpose, openedAt, timeOfUsing, address) VALUES (?,?,?,?,?,?,?)"
+         resource
+  putStrLn "resource inserted"
+
+getAllUsers :: Connection -> IO [User]
+getAllUsers conn = query_ conn "SELECT * FROM users"
+
+insertUser :: Connection -> User -> IO ()
+insertUser conn user = do
+  _ <- execute conn
+         "INSERT INTO users (username) VALUES (?)"
+         user
+  putStrLn "user inserted"
