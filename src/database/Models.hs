3634f3972554aba4db1287406e38a32f251497b9
vhorielova <horielova.viktoria@gmail.com>
2025-10-09 16:08:52 +0300

    add FromRow and ToRow instances

--- src/database/Models.hs
+++ src/database/Models.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Models where
 
 import GHC.Generics (Generic)
 import Data.Time (UTCTime)
+import Database.PostgreSQL.Simple.FromRow
+import Database.PostgreSQL.Simple.ToRow
+import Database.PostgreSQL.Simple.ToField (ToField(..))
 
 data Resource = Resource {   
     resourceId :: Int,
@@ -16,10 +20,34 @@ data Resource = Resource {
     address :: String
 } deriving (Show, Generic)
 
+instance FromRow Resource where
+    fromRow = Resource <$> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field
+
+instance ToRow Resource where
+    toRow r = [
+        toField (resourceId r),
+        toField (title r),
+        toField (annotation r),
+        toField (typeId r),
+        toField (purpose r),
+        toField (openedAt r),
+        toField (timeOfUsing r),
+        toField (address r),
+    ]
+
 data User = User {   
     userId :: Int,
     username :: String,
 } deriving (Show, Generic)
+
+instance FromRow User where
+    fromRow = User <$> field <*> field
+
+instance ToRow User where
+    toRow u = [
+        toField (userId u),
+        toField (username u)
+    ]
 
 data Author = Author {
     authorId :: Int,
